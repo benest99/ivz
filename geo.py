b82f6ea8440069f85bc3b01353e0036ba3c0c9c8
benest99 <benest.radesin@gmail.com>
2021-12-23 02:10:12 +0100

    update

--- geo.py
+++ geo.py
@@ -6,18 +6,32 @@ import matplotlib.pyplot as plt
 import contextily
 import sklearn.cluster
 import numpy as np
+
+import pickle
 # muzete pridat vlastni knihovny
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
-    pass
+    df = df.loc[(df["d"] != None )|( df["e"]!=None)]
+
+    return geopandas.GeoDataFrame(
+        df, 
+        geometry=geopandas.points_from_xy(df["d"], df["e"]),
+        crs = "EPSG:5514"
+        )
+    
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s sesti podgrafy podle lokality nehody 
      (dalnice vs prvni trida) pro roky 2018-2020 """
-    pass
+    # Z gdf vybrat rok 2018-2020
+
+    gdf.plot()
+
+
+    plt.savefig("obrazek.png")
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
